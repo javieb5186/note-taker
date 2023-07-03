eabb58d69eee5873200e8c27dd55fed09e02c18f
javieb5186 <javieb5186@gmail.com>
2023-07-02 20:47:05 -0600

    Add modular routing

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index');
 
 const app = express();
+app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
