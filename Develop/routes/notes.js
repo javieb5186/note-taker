674d83ab2ef8ae1c658b903cc199e0973d4e3ea1
javieb5186 <javieb5186@gmail.com>
2023-07-02 21:19:22 -0600

    Add unique ids to each saved note

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const db = require('../db/db.json');
 const {writeFile} = require('fs');
+const {v4: uuidv4} = require('uuid');
+
 
 router.get('/', (req, res) => {
   res.json(db);
@@ -9,11 +11,28 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   console.log(req.body);
 
-  db.push(req.body);
+  let keyIsUnique = true;
+  let key;
+
+  do {
+    key = uuidv4();
+
+    for (let i = 0; i < db.length; i++) {
+      if(db[i].id === key)
+        keyIsUnique = false;
+    }
+  } while (!keyIsUnique);
+
+  const object = req.body;
+  object.id = key;
+
+  db.push(object);
   writeFile('./db/db.json', JSON.stringify(db), (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
 });
+
+
 
 module.exports = router;
